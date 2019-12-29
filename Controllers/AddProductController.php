39c7799396893e0082b32d78982dc023913765be
Abdallah Naguib <abdonaguib99@gmail.com>
2019-12-29 05:26:15 +0200

    pulling

---
+++ Controllers/AddProductController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdo
+ * Date: 12/29/19
+ * Time: 5:19 AM
+ */
+include '../Models/Product.php';
+$product=new Product();
+$product->name=$_POST['name'];
+$product->des=$_POST['des'];
+$product->price=$_POST['price'];
+$product->quantity=$_POST['quantity'];
+$product->image=$_POST['image'];
+$product->save();
+header("http://localhost/ITProject-master/Views/UpdateProductsView.php");
---
+++ Controllers/UpdateProduct.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdo
+ * Date: 12/29/19
+ * Time: 5:19 AM
+ */
